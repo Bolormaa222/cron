e35b386df92aea38a42956587e451263e6dabf60
Bolormaa222 <bolormaaenkhbat222@gmail.com>
2024-10-19 17:40:01 +0800

    commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var cron = require('node-cron');
+const express = require('express');
+const app = express ();
+app.use(express.json());
+
+app.get("/status", (request, response) => {
+    const status = {
+       "Status": "Running"
+    };
+    
+    response.send(status);
+});
+
+app.listen(8080, () => {
+    console.log("Server Listening on PORT:", port);
+});
+
+cron.schedule('* * * * *', async () => {
+  console.log('running every minute 1, 2, 4 and 5');
+  const result = await fetch("https://cvback-yj18.onrender.com/",{
+    method:"GET"
+  });
+  let res = await result.json()
+  console.log("res ", res)
+});
